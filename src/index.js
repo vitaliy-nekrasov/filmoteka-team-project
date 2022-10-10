2657c1fee4289fc0826d2f0b06ff1f1afb418714
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-10-11 02:58:35 +0300

    footer-modal

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './js/header';
 import './js/library';
 import './js/renderPage';
 import './js/gallery-lib';
-
+import './js/team';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
