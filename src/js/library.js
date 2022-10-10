2612d07fe8aa896fd9cc68d32b0cf26bc9b64dad
Volodymyr Dzyura <103021185+volodymyrdzyura@users.noreply.github.com>
2022-10-10 08:24:39 +0300

    fix 12
    додав js на кнопки

--- src/js/library.js
+++ src/js/library.js
@@ -1,7 +1,20 @@
 const refs = {
     watched: document.querySelector('.watched'),
-    queue: document.querySelector('.queue')
+    queue: document.querySelector('.queue'),
+    enLang: document.querySelector('.en'),
+    ukLang: document.querySelector('.uk')
 }
+
+refs.enLang.addEventListener('click', ()=>{
+  refs.enLang.classList.add('activeLang');
+  refs.ukLang.classList.remove('activeLang');
+});
+
+refs.ukLang.addEventListener('click', ()=>{
+  refs.ukLang.classList.add('activeLang');
+  refs.enLang.classList.remove('activeLang');
+});
+
 
 try {
     refs.watched.addEventListener('click', () => {
@@ -20,14 +33,3 @@ try {
   } catch (eror) {
     console.log(eror);
   }
-
-
-// refs.watched.addEventListener('click', ()=>{
-//     refs.watched.classList.add('active');
-//     refs.queue.classList.remove('active');
-// });
-
-// refs.queue.addEventListener('click', ()=>{
-//     refs.queue.classList.add('active');
-//     refs.watched.classList.remove('active');
-// });
